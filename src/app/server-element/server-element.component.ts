c15285b074789b32bc75a2bb710ce3ecfe467c29
KMP <krzysztof.mikolaj.przybylski@gmail.com>
2019-06-14 21:09:46 +0200

    .

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, EventEmitter, Output, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Server } from '../shared/server.model';
 
 @Component({
@@ -6,18 +6,22 @@ import { Server } from '../shared/server.model';
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit, OnDestroy {
+export class ServerElementComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @Input() serverElement: Server;
   @Output() deleteCalled = new EventEmitter<Server>();
+  @ViewChild('heading', {static: true}) header: ElementRef;
 
   constructor() {
     console.log('constructor called!');
    }
 
    ngOnInit() {
-     console.log('ngOnInint called!');
 
+   }
+
+   ngAfterViewInit() {
+    console.log('Text Content ' + this.header.nativeElement.textContent);
    }
 
    ngOnDestroy() {
